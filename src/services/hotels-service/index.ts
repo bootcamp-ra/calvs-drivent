9268099aa84383a661aa1bc77c76f71e131e9e7f
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-12-18 19:32:09 -0300

    feat: GET /hotels/:hotelId now returns roomTypes attribute

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,6 +3,7 @@ import enrollmentRepository from "@/repositories/enrollment-repository";
 import ticketRepository from "@/repositories/ticket-repository";
 import { notFoundError } from "@/errors";
 import { cannotListHotelsError } from "@/errors/cannot-list-hotels-error";
+import { Hotel, Room } from "@prisma/client";
 
 async function listHotels(userId: number) {
   //Tem enrollment?
@@ -29,10 +30,37 @@ async function getHotelsWithRooms(userId: number, hotelId: number) {
   await listHotels(userId);
   const hotel = await hotelRepository.findRoomsByHotelId(hotelId);
 
-  if (!hotel) {
+  if(!hotel) {
     throw notFoundError();
   }
-  return hotel;
+
+  const roomTypes = hotelRoomTypes(hotel);
+
+  return {
+    ...hotel,
+    roomTypes,
+  };
+}
+
+function hotelRoomTypes(hotel: Hotel & {
+  Rooms: Room[];
+}) {
+  function hotelCapacity(array: Room[], x: number, l=0, r=array.length-1): boolean {
+    if(l > r) return false; 
+    const mid = Math.floor(l+r/2);
+    if(array[mid].capacity === x) return true;
+    
+    if(array[mid].capacity > x) return hotelCapacity(array, x, l, r=mid-1);
+    if(array[mid].capacity < x) return hotelCapacity(array, x, l=mid+1, r);
+  }
+
+  const rooms = hotel.Rooms;
+
+  return {
+    single: hotelCapacity(rooms, 1),
+    double: hotelCapacity(rooms, 2),
+    triple: hotelCapacity(rooms, 3),
+  };  
 }
 
 const hotelService = {
