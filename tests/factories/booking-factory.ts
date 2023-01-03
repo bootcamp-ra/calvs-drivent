e114c1ca2a9b48ce60c8b9e28c108471b2ac03d8
guilhermeicarofr <guilhermeicarofr@gmail.com>
2023-01-03 17:54:59 -0300

    test: some booking and hotels test fixes and code cleaning for better test coverage and organization

--- tests/factories/booking-factory.ts
+++ tests/factories/booking-factory.ts
@@ -3,15 +3,13 @@ import { prisma } from "@/config";
 type CreateBookingParams = {
   roomId: number,
   userId: number,
-  people: number
 }
 
-export function createBooking({ roomId, userId, people }: CreateBookingParams) {
+export function createBooking({ roomId, userId }: CreateBookingParams) {
   return prisma.booking.create({
     data: {
       userId,
       roomId,
-      people,
     }
   });
 }
