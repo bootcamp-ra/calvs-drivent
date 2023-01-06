d8d9840dbbf9617c9d712dfd9bd01842910d1288
Tukkos <jpfcastro@id.uff.br>
2023-01-06 10:48:34 -0300

    test: get space, get  activities and get activities booking

--- tests/factories/activities-factory.ts
+++ tests/factories/activities-factory.ts
@@ -17,3 +17,25 @@ export async function createActivitiesSpace() {
     },
   });
 }
+
+export async function createActivitie(dateId: number, spaceId: number) {
+  return await prisma.activities.create({
+    data: {
+      name: "Palestra A",
+      capacity: Number(faker.random.numeric(2)),
+      dateId: dateId,
+      spaceId: spaceId,
+      duration: Number(faker.random.numeric()),
+      start: 9,
+    },
+  });
+}
+
+export async function createActivitieBooking(activitieId: number, ticketId: number) {
+  return await prisma.activitiesBooking.create({
+    data: {
+      activitieId: activitieId,
+      ticketId: ticketId,
+    },
+  });
+}
