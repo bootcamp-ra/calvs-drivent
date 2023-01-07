efd247aac557344ecd8f2b60379c119c805d777d
luciano <lpereira2306@gmail.com>
2023-01-07 09:34:45 -0300

    add: oauth github login

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -2,8 +2,12 @@ import userService from "@/services/users-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
+import authenticationService, { SignInParams } from "@/services/authentication-service";
+
 export async function usersPost(req: Request, res: Response) {
   const { email, password } = req.body;
+
+  console.log(email + password);
 
   try {
     const user = await userService.createUser({ email, password });
@@ -18,3 +22,29 @@ export async function usersPost(req: Request, res: Response) {
     return res.status(httpStatus.BAD_REQUEST).send(error);
   }
 }
+
+// export async function usersSignUpSignInWithGit(req: Request, res: Response) {
+//   const { email, accessToken } = req.body;
+//   console.log('entrou');
+//   console.log(email + accessToken);
+
+//   try {
+//     const user = await userService.validateUniqueEmailOrFailGit(email);
+
+//     console.log(user);
+
+//     if (user) {
+//       const result = await authenticationService.signInWithGit(email, accessToken);
+//       return res.status(httpStatus.OK).send(result);
+//     }
+
+//     const user2 = await userService.createUserWithGit(email);
+//     const result = await authenticationService.signInWithGit(email, accessToken);
+//     return res.status(httpStatus.OK).send(result);
+//   } catch (error) {
+//     if (error.name === 'DuplicatedEmailError') {
+//       return res.status(httpStatus.CONFLICT).send(error);
+//     }
+//     return res.status(httpStatus.BAD_REQUEST).send(error);
+//   }
+// }
